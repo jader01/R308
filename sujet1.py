f84e3ba9ca9b5ac9695b942ae73581b6919fa029
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-09-22 13:31:40 +0200

    complet jusqu'au compte d'étudiant dans la promo sujet1.py

--- sujet1.py
+++ sujet1.py
@@ -17,59 +17,65 @@ nompromo="but2"
 
 
 def createtudiant(nom, prenom): #fonction pour crée un étudiant
-    print("lancement creation etudiant.....")
+    print("lancement creation etudiant.....") #print pour vérif
     etudiant={ #creation du dictionaire dans le quel on stock les étudiants
         "nom": nom, #nom de l'étudiant a qui on associe une variable pour commencer
         "prenom" : prenom, #prénom de l'étudiant au quel on associe la variable pour commencer
-        "notes" : []
+        "notes" : [] #ici on crée un tableau vide au quel on associera le tuple note + coef après
     }
     #print(etudiant)
-    print("etudiant créée\n")
+    print("etudiant créée\n")#print pour vérif
     return(etudiant)
 
 
 def ajouternotes(etudiant, note, coef): #creation de la liste des étudiants lier avec les notes
-    print("ajout des notes de l'étudiant.........")
+    print("ajout des notes de l'étudiant.........")#print pour vérif
     etudiant['notes'].append((note, coef)) #ici on associe la note et le coef rentré précédement a la variable "notes" dans la table étudiant
-    print("etudiant et notes : ", etudiant, "\n")
+    print("etudiant et notes : ", etudiant, "\n")#print pour vérif
     return(etudiant)
 
 def totalnotes(dico):
-    print("calcul total de note \n")
+    print("calcul total de note \n")#print pour vérif
     return len(dico["notes"]) #ici on calcule le nombre de notes présente dans le dictionnaire au total, pour ensuite pouvoir calculer la moyenne
 
 
 def creationpromo(): #creation d'une promo 
-    print("creation de la promo.................")
-    promo = {
-        "nom de la promo" : "but 2 PILPRO",
-        "etudiant de la promo" : []
+    print("creation de la promo.................") #print pour vérif
+    promo = { #création du dico promo
+        "nom de la promo" : "but 2 PILPRO", #nom de la promo sous format texte
+        "etudiant de la promo" : [] #création d'un tableau au quel on va associer les etudiant de la promo et leur note trouvé précédement
     }
-    print("promo créée..........")
+    print("promo créée..........\n") #print pour vérif
     return(promo)
 
 #moyenne
 def fairemoyenne(liste): # on crrée une fonction de calcule de moyenne avec en paramètre la liste complete généré précédement
-    additionnotes=0
-    addcoef=0
-    for elm in liste['notes']:
-        print("calcule de la moyenne............")
+    additionnotes=0 #varriable à 0 que l'on va utiliser plus tard pour le calcul de moyenn
+    addcoef=0 #varriable à 0 que l'on va utiliser plus tard pour le calcul de moyenn
+    for elm in liste['notes']: #ici on parcours tous les élément dans le dictionnaire étudiant mais on parcours seulement leur 'notes'
+        print("calcule de la moyenne............") #print pour vérif
         addcoef = addcoef + elm[1]
         additionnotes = additionnotes + (elm[0]*elm[1])
-        print("la moyenne est : ", additionnotes/addcoef, "\n")
+        print("la moyenne est : ", additionnotes/addcoef, "\n") #print pour vérif
     return additionnotes/addcoef
 
-def addetudiantpromo(promo, etudiant):
-    print("association de l'étudiant a une promotion")
-    promo["etudiant de la promo"].append(etudiant)
+def addetudiantpromo(promo, etudiant): #cette fonction permettra d'ajouter un étudiant a la promotion
+    print("association de l'étudiant a une promotion") #print pour vérif
+    promo["etudiant de la promo"].append(etudiant) #ici on ajoute l'étudiant a la variable sous forme de liste créé dans le dico
     print(promo)
+    print("l'étudiant a été associer\n") #print pour vérif
+    return(promo)
 
+def totalpromo(promo): #cette fonction va parmettre de compter le nomre d'étudiant présent dans une promo
+    print("compte du nombre d'étudiant............") #print pour vérif
+    print("il y a : ", len(promo['etudiant de la promo']), "étudiant(s) dans la promo", promo['nom de la promo'])  #print pour vérif de tout les détails souhaiter
+    return(len(promo['etudiant de la promo'])) #print pour vérif
 
 ################################################################################
 #                              apl de fonctions
 ################################################################################
 
-resultetudiant=createtudiant("rueda lucantis", "jade")
+resultetudiant=createtudiant("rueda lucantis", "jade") 
 
 etudiantetnotes=ajouternotes(resultetudiant, int(15), int(2))
 
@@ -77,5 +83,6 @@ fairemoyenne(etudiantetnotes)
 
 promo=creationpromo()
 
-addetudiantpromo(promo, etudiantetnotes)
+promocomplette=addetudiantpromo(promo, etudiantetnotes)
 
+totalpromo(promocomplette)
