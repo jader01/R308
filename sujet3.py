12026ba996142128dbe73501c6488af6f07afeae
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-09-26 13:50:29 +0200

    Add files via upload

---
+++ sujet3.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    def printNode(self):
+        print(self.data, end= " ")
+        if self.next is not None:
+            self.next.printNode()
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printListRec(self):
+        if self.head is not None:
+            self.head.printNode()
+        print()
+
+
+myLinkedList = LinkedList()
+myNode1 = Node(10)
+myNode2 = Node(20)
+myNode3 = Node(30)
+myNode4 = Node(40)
+myLinkedList.head = myNode1
+myNode1.next = myNode2
+myNode2.next = myNode3
+myNode3.next = myNode4
+print("The elements in the linked list are:")
+myLinkedList.printListRec()
