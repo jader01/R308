dda2f30d594cf75879ac2c5e350b53606c213739
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-10-03 16:22:54 +0200

    debut association sujet4.py

--- sujet4.py
+++ sujet4.py
@@ -6,4 +6,33 @@ class Node :
     def __init__(self, valeur, gauche=None, droite=None): #initialisation du noeuf
         self.valeur = valeur #valeur que l'on associera
         self.gauche = gauche #enfant de gauche
-        self.droit = droite #enfant de droite
+        self.droite = droite #enfant de droite
+
+
+class Tree :
+    def __init__(self):
+        self.head = None
+
+
+
+################################################################################
+#                    association des valeurs a des variables
+################################################################################
+
+myLinkedList = Tree()
+myNode1 = Node(10)
+myNode2 = Node(20)
+myNode3 = Node(30)
+myNode4 = Node(15)
+myNode5 = Node(40)
+
+
+########################################################################
+#                           association dans l'arbre
+#######################################################################
+
+myLinkedList.head = myNode1
+myNode1.gauche = myNode2
+myNode2.droite = myNode3
+myNode3.gauche = myNode4
+myNode3.droite = myNode5
