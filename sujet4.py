0aef1c1c23a11313d515b60adf7d2634f651df5f
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-10-08 12:22:38 +0200

    affichage sujet4.py

--- sujet4.py
+++ sujet4.py
@@ -8,10 +8,23 @@ class Node :
         self.gauche = gauche #enfant de gauche
         self.droite = droite #enfant de droite
 
+    def printeNode2(self): #affichage de toutes les valeur en parcourant tout l'arbre
+        print(self.valeur) #ici ont affiche la valeur
+        if self.gauche is not None : #si il y a quelque chose a gauche
+            return self.gauche.printeNode2() #alors on rapelle la fonction et gauche deviens le debut rt on retourne la valeur
+
+        if self.droite is not None : #si il y a quelque chose droite
+            return self.droite.printeNode2() #alors on rapelle la fonction et gauche deviens le debut rt on retourne la valeur
+
+
 
 class Tree :
     def __init__(self):
-        self.head = None
+        self.head = None #on initialise la te de la liste
+    
+    def printTree(self) :
+        if self.head is not None : #si la tete n'est pas vide
+            self.head.printeNode2() #on apelle la fonction printNode2 ci dessus
 
 
 
@@ -36,3 +49,9 @@ myNode1.gauche = myNode2
 myNode2.droite = myNode3
 myNode3.gauche = myNode4
 myNode3.droite = myNode5
+
+########################################################################
+#                           apl des fonctions
+#######################################################################
+
+myLinkedList.printTree()
